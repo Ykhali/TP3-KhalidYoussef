5aea58199838099e82605651142ccde2f355ff83
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-11-10 21:28:48 +0100

    initial commit

---
+++ src/main/java/emsi/khalid/tp2webkhalidyoussef/tp3khalidyoussef/HelloApplication.java
@@ -0,0 +1,9 @@
+package emsi.khalid.tp2webkhalidyoussef.tp3khalidyoussef;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/emsi/khalid/tp2webkhalidyoussef/tp3khalidyoussef/HelloResource.java
@@ -0,0 +1,14 @@
+package emsi.khalid.tp2webkhalidyoussef.tp3khalidyoussef;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
